c957ea5fce21660f95005fd330449531e1a99606
Wu <jio12138@qq.com>
2024-12-17 17:37:01 +0800

    init win codes

--- src/go_maze.c
+++ src/go_maze.c
@@ -3,3 +3,15 @@
 //
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
+
+int main(){
+    while(1){
+        printf("hello world\n");
+        getch();
+        if(getch() == 'q'){
+            break;
+        }
+    }
+    return 0;
+}
