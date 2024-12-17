fa8d01d526a17ab9e78c186345a4eca469df69db
Wu <jio12138@qq.com>
2024-12-17 17:50:39 +0800

    init ubuntu codes

--- src/go_maze.c
+++ src/go_maze.c
@@ -2,16 +2,4 @@
 // Created by wu on 24-12-16.
 //
 #include <stdio.h>
-#include <string.h>
-#include <conio.h>
-
-int main(){
-    while(1){
-        printf("hello world\n");
-        getch();
-        if(getch() == 'q'){
-            break;
-        }
-    }
-    return 0;
-}
+#include <ncurses.h>
